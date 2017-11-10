efe1daf32d29e7ad9720f5e51ce59773e2f23459
Kimouss <kim.quang.p@gmail.com>
2017-11-10 13:09:01 +0100

    test one

---
+++ src/components/SmartAngels.vue
@@ -0,0 +1,6 @@
+<template>
+  <p>{{ msg }}</p>
+
+</template>
+
+<script src="../js/SmartAngels.js"></script>
---
+++ src/js/SmartAngels.js
@@ -0,0 +1,6 @@
+let SmartAngels = new Vue({
+  el: '#test',
+  data: {
+    msg: 'Toto sur un bato :D'
+  }
+})
